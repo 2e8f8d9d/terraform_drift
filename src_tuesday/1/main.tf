50b15fcbc19239e506a8b8552b639ea2957df9b5
Caige Kelly <Caige.Kelly@insight.com>
2021-03-16 16:05:05 +0000

    Updated main.tf

--- src_tuesday/1/main.tf
+++ src_tuesday/1/main.tf
@@ -10,7 +10,10 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+
+  subscription_id = "99c19e8a-0929-4a39-af63-a232d1686654"
 }
+
 resource "azurerm_resource_group" "my_resource_group" {
     name = "terraform-rg"
     location = "centralus"
